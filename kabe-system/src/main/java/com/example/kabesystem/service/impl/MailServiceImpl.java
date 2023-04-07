ad689602de12eeaee7816944664c40c581a817c0
Pluck <2502000418@qq.com>
2023-04-08 00:48:41 +0800

    impl method VerificationCodeServiceImpl.generateVerificationCode()

---
+++ kabe-system/src/main/java/com/example/kabesystem/service/impl/MailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.kabesystem.service.impl;
+
+import com.example.kabesystem.service.MailService;
+import org.springframework.beans.factory.annotation.Value;
+
+public class MailServiceImpl implements MailService {
+
+
+    @Override
+    public void sendMail(String to, String subject, String verifyCode) {
+
+    }
+}
---
+++ kabe-system/src/main/java/com/example/kabesystem/service/impl/VerificationCodeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.kabesystem.service.impl;
+
+import com.example.kabesystem.service.VerificationCodeService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class VerificationCodeServiceImpl implements VerificationCodeService {
+
+    @Value("${mail.verification-code.length}")
+    private Integer verificationCodeLength;
+
+    private final String[] metaCode = {"0", "1", "2", "3", "4", "5", "6", "7", "8",
+            "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    @Override
+    public String generateVerificationCode() {
+        Random random = new Random();
+        StringBuilder verificationCode = new StringBuilder();
+        while (verificationCode.length() < verificationCodeLength) {
+            int i = random.nextInt(metaCode.length);
+            verificationCode.append(metaCode[i]);
+        }
+        return verificationCode.toString();
+    }
+}
