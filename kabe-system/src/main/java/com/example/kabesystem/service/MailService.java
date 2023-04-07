0b53a1e2d6fe68e4373dfa0cfddc769ff976fd41
Pluck <2502000418@qq.com>
2023-04-08 06:05:05 +0800

    实现了 MailService.sendVerificationMail 方法，完成了发送验证邮件的功能。 目前发送的邮件样式需要改善（目前只是简单地使用了 HMLT5 的 h1, div 等标签，没用上 CSS 改善样式）。

--- kabe-system/src/main/java/com/example/kabesystem/service/MailService.java
+++ kabe-system/src/main/java/com/example/kabesystem/service/MailService.java
@@ -1,11 +1,5 @@
 package com.example.kabesystem.service;
 
 public interface MailService {
-    /**
-     * 发送邮件
-     * @param to 邮件收件人
-     * @param subject 邮件主题
-     * @param verifyCode 邮件验证码
-     */
-    public void sendMail(String to, String subject, String verifyCode);
+    public boolean sendVerificationMail(String addressee, String username);
 }
